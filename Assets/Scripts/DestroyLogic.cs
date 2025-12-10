b686d8c2cae40ac97be839420da5128050f9291a
Stratossalp <stratosjunior02@gmail.com>
2025-12-10 13:50:03 +0200

    Piou(Raycast) & Health

--- Assets/Scripts/DestroyLogic.cs
+++ Assets/Scripts/DestroyLogic.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
+using PurrNet;
+using System;
 
-public class AutoDestroy : MonoBehaviour
+public class AutoDestroy : NetworkBehaviour
 {
-    public float lifetime = 5f;
+    [SerializeField] private float lifetime = 5f;
 
     void Start()
     {
         Destroy(gameObject, lifetime);
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (isHost)
+    //    {
+    //        DestroyBullet();
+    //    }
+    //}
+
+    [ServerRpc]
+    void DestroyBulletServerRpc()
+    {
+        DestroyBullet();
+    } 
+
+    private void DestroyBullet()
+    {
+        NetworkIdentity netId = GetComponent<NetworkIdentity>();
+        if (netId != null)
+        {
+            netId.Despawn(); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/ShootingLogic.cs
+++ Assets/Scripts/ShootingLogic.cs
@@ -2,28 +2,162 @@
 
 using System;
 using UnityEngine;
+using PurrNet;
 
-public class ShootingLogic : MonoBehaviour
+
+public class ShootingLogic : NetworkBehaviour
 {
-    public Transform firePoint;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject bulletTrail;
+    [SerializeField] private float weaponRange = 10f;
+    [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private int damage = 20;
+
+
+    protected override void OnSpawned()
+    {
+        base.OnSpawned();
+
+        enabled = isOwner;
+    }
+
+    private void Update()
+    {
+        if (!isOwner)
+            return;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(firePoint.position, transform.up, weaponRange, hitLayer);
+            var trail = Instantiate(bulletTrail, firePoint.position, transform.rotation);
+            var trailScript = trail.GetComponent<BulletTrail>();
+
+            if (hit.collider != null)
+            {
+                Debug.Log($"Hit: {hit.transform.name}");
+                trailScript.SetTrargetPosition(hit.point);
+
+                if (hit.transform.TryGetComponent(out PlayerHealth playerHealth))
+                {
+                    playerHealth.ChangeHealth(-damage);
+                }
+
+            }
+            else
+            {
+                Debug.Log("No hit");
+                var targetPosition = firePoint.position + transform.up * weaponRange;
+                trailScript.SetTrargetPosition(targetPosition);
+            }
+
+
+            NetworkIdentity netId = trail.GetComponent<NetworkIdentity>();
+            netId.Spawn(null);
+
+        }
+    }
+}
+
+/////////// RAYCAST SOLUTION //////////
+/*
+ [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject bulletTrail;
+    [SerializeField] private float weaponRange = 10f;
+    [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private int damage = 20;
+   
+
+    protected override void OnSpawned()
+    {
+        base.OnSpawned();
+
+        enabled = isOwner;
+    }
+
+    private void Update()
+    {
+        if (!isOwner)
+            return;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(firePoint.position, transform.up, weaponRange, hitLayer);
+            var trail = Instantiate(bulletTrail, firePoint.position, transform.rotation);
+            var trailScript = trail.GetComponent<BulletTrail>();
+
+            if (hit.collider != null)
+            {
+                Debug.Log($"Hit: {hit.transform.name}");
+                trailScript.SetTrargetPosition(hit.point);
+
+                if (hit.transform.TryGetComponent(out PlayerHealth playerHealth))
+                {
+                    playerHealth.ChangeHealth(-damage);
+                }
+                  
+            }
+            else
+            {
+                Debug.Log("No hit");
+                var targetPosition = firePoint.position + transform.up * weaponRange;
+                trailScript.SetTrargetPosition(targetPosition);
+            }
+
+      
+            NetworkIdentity netId = trail.GetComponent<NetworkIdentity>();
+            netId.Spawn(null);
+
+        }
+    }
+ */
+
+
+
+
+
+
+
+///////// ADD FORCE SOLUTION /////////////
+/*
+  public Transform firePoint;
     public GameObject bulletPrefab;
 
     public float bulletForce = 20f;
 
+    protected override void OnSpawned()
+    {
+        base.OnSpawned();
+
+        enabled = isOwner;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (!isOwner)
+            return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Shoot();
+            ShootServerRpc();
         }
     }
 
-    void Shoot()
+
+    [ServerRpc]
+    void ShootServerRpc()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-    }
 
-}
+        NetworkIdentity netId = bullet.GetComponent<NetworkIdentity>();
+        netId.Spawn(null);
+    }
+ */
+
+
+
