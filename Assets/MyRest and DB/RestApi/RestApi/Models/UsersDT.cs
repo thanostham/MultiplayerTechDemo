3918a99c49dd0de577f57b0afdbeab7d239bd686
thanostham <thanostham@gmail.com>
2026-01-03 17:21:28 +0200

    Revert "moved rest api inside project to be more accessible"
    This reverts commit 526972bcfd83304eba06dfd7e1668dfaba797162.

--- Assets/MyRest and DB/RestApi/RestApi/Models/UsersDT.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestApi.Models
-{
-    public class UsersDT
-    {
-        public string Id { get; set; }
-        public string? Name { get; set; }
-        public int skin_ID { get; set; }
-    }
-}
